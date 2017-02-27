62d9116139dbec780d9e22079b0d358dd77aeb6b
Andreas  Berger <office@andreasberger.at>
2017-02-27 10:29:47 +0100

    - added documentation

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,19 +2,32 @@
   var inpPlaylist = document.getElementById("playlist");
   var btnStream = document.getElementById("stream");
 
+  /**
+   * @summary This function redirects the user to the stream page
+   * @param value
+   */
   function redirect(value) {
     var param = encodeURIComponent(value);
     window.location.href = "/stream.html?url=" + param
   }
 
+  /**
+   * @summary Redirect on url change
+   */
   function urlChange() {
     redirect(inpPlaylist.value);
   }
 
+  /**
+   * @summary Redirect on click
+   */
   function streamClick() {
     redirect(inpPlaylist.value);
   }
 
+  /**
+   * @summary Append listeners to elements
+   */
   inpPlaylist.addEventListener("change", urlChange);
   btnStream.addEventListener("click", streamClick);
 })();
